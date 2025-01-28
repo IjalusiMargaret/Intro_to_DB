5bb655fe1cef0c90c612e0f3cf82f5ea5383943b
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-01-28 16:18:47 +0100

    fix

---
+++ task_6.sql
@@ -0,0 +1,12 @@
+-- Use the specified database
+USE alx_book_store;
+
+-- Remove existing data (optional)
+DELETE FROM customers;
+
+-- Insert multiple rows into the 'customers' table
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES
+    (2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+    (3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+    (4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
