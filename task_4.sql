a996e21cf16c0c769b03dd584c3e91d11d2be18b
Ijalusi Margaret <ijalusimargaret@gmail.com>
2025-02-03 20:22:36 +0100

    fix

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,12 @@
 -- Use the specified database
 USE alx_book_store;
 
--- Query to get detailed information about the columns in the 'books' table
-SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, EXTRA
+
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_DEFAULT, COLUMN_KEY, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
+
+
+
+9
